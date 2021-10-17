0094401a65b1478db4f8843f6f65b10fe4adfb13
pranavvadanere <pranav.vadanere@gmail.com>
2021-10-17 21:17:24 +0530

    first commit

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,16 +5,39 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
+     private StringCalculator calculator;
+     @Rule
+    public ExpectedException thrown = ExpectedException.none();
+     @Before
+    public void initialize() {
+        calculator = new StringCalculator();
+    }
+
 
     @Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, calculator.add(""));
+    }
+        @Test
+    public void numberStringShouldReturnSameNumber() {
+        assertEquals(calculator.add("1"), 1);
+        assertEquals(calculator.add("5"), 5);
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+     @Test
+    public void numbersCommaDelimitedShouldBeSummed() {
+        assertEquals(calculator.add("1,2"), 3);
+        assertEquals(25, calculator.add("10,15"));
+    }
+
+    @Test
+    public void numbersNewlineDelimitedShouldBeSummed() {
+        assertEquals(calculator.add("1\n2"), 3);
+        assertEquals(calculator.add("11\n13"), 24);
     }
 }
